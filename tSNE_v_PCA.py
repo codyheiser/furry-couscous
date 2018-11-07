b955c43f1b02480a5d097c666089d3d8bd7ab1f8
Cody Heiser <codyheiser49@gmail.com>
2018-11-07 13:19:35 -0600

    updating results of initial PCA v tSNE testing

--- tSNE_v_PCA.py
+++ tSNE_v_PCA.py
@@ -9,6 +9,7 @@ import scipy as sc
 # scikit packages
 import skbio
 from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
 # import tsne package
 import sys; sys.path.append('/Users/Cody/git/FIt-SNE')
 from fast_tsne import fast_tsne
@@ -38,15 +39,8 @@ for rep in reps.keys():
         print('performing PCA for rep {} with closeness {}'.format(rep,key))
         pca = PCA(n_components=50).fit_transform(np.log2(reps[rep][key]+1)) # perform PCA with 50 components
         
-        # plot PCA
-        plt.figure(figsize=(5,5))
-        sns.scatterplot(pca[:,0], pca[:,1], s=75)
-        plt.title('PCA {} {}'.format(rep,key))
-        plt.xlabel('Component 1')
-        plt.ylabel('Component 2')
-        sns.despine(left=True, bottom=True)
-        plt.tight_layout()
-        plt.savefig('outputs/PCA_{}_{}.pdf'.format(rep,key))
+        print('clustering PCA results using k-means')
+        pca_clust = KMeans(n_clusters=2).fit_predict(pca[:,1:3]) # TODO: figure out why I need to slice [:,1:3]
         
         print('generating PCA distance matrix for rep {} with closeness {}'.format(rep,key))
         dm_pca = sc.spatial.distance_matrix(x=pca,y=pca) # generate distance matrix for PCA
@@ -54,13 +48,8 @@ for rep in reps.keys():
         print('performing t-SNE for rep {} with closeness {}'.format(rep,key))
         tsne = fast_tsne(pca,seed=18,perplexity=30) # perform tSNE with perplexity 30 using PCA output
         
-        # plot tSNE
-        plt.figure(figsize=(5,5))
-        sns.scatterplot(tsne[:,0], tsne[:,1], s=75)
-        plt.title('t-SNE {} {}'.format(rep,key))
-        sns.despine(left=True, bottom=True)
-        plt.tight_layout()
-        plt.savefig('outputs/tSNE_{}_{}.pdf'.format(rep,key))
+        print('clustering t-SNE results using k-means')
+        tsne_clust = KMeans(n_clusters=2).fit_predict(tsne)
         
         print('generating t-SNE distance matrix for rep {} with closeness {}'.format(rep,key))
         dm_tsne = sc.spatial.distance_matrix(x=tsne,y=tsne) # generate distance matrix for tSNE
@@ -71,5 +60,34 @@ for rep in reps.keys():
         corr['Closeness']=key
         
         corr_out = corr_out.append(corr)
+        
+        print('plotting results')
+        # plot results
+        plt.figure(figsize=(15,5))
+        
+        # plot PCA
+        plt.subplot(131)
+        sns.scatterplot(pca[:,0], pca[:,1], s=75, hue=pca_clust)
+        plt.title('PCA {} {}'.format(rep,key))
+        plt.xlabel('Component 1')
+        plt.ylabel('Component 2')
+        
+        # plot tSNE with PCA clusters
+        plt.subplot(132)
+        sns.scatterplot(tsne[:,0], tsne[:,1], s=75, hue=pca_clust)
+        plt.title('t-SNE with PCA clusters {} {}'.format(rep,key))
+        plt.xlabel('t-SNE 1')
+        plt.ylabel('t-SNE 2')
+        
+        # plot tSNE with tSNE clusters
+        plt.subplot(133)
+        sns.scatterplot(tsne[:,0], tsne[:,1], s=75, hue=tsne_clust)
+        plt.title('t-SNE {} {}'.format(rep,key))
+        plt.xlabel('t-SNE 1')
+        plt.ylabel('t-SNE 2')
+        
+        sns.despine(left=True, bottom=True)
+        plt.tight_layout()
+        plt.savefig('outputs/PCA_v_tSNE_{}_{}.pdf'.format(rep,key))
         
 corr_out.to_csv('outputs/tSNE_PCA_correlations.csv')
