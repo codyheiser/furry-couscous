e378e7b68cfae6584a5cdaab3681a0a48c423a29
Cody Heiser <codyheiser49@gmail.com>
2018-11-19 12:15:33 -0600

    figuring out DCA, added to utilities file along with automated PCA and tSNE wrapper functions

--- fcc_utils.py
+++ fcc_utils.py
@@ -8,9 +8,14 @@ import scipy as sc
 # scikit packages
 import skbio
 from sklearn.preprocessing import normalize
+from sklearn.decomposition import PCA        # PCA
+from sklearn.manifold import TSNE            # t-SNE
+# DCA packages
+import scanpy.api as scanpy
+from dca.api import dca                      # DCA
 # plotting packages
 import matplotlib.pyplot as plt
-import seaborn as sns; sns.set(style = 'whitegrid')
+import seaborn as sns; sns.set(style = 'white')
 # package for reading in data files
 import h5py
 
@@ -24,7 +29,7 @@ def read_hdf5(filename):
     hdf5in.close()
     return hdf5out
 
-def corr_distances(mat1, mat2, plot_out = True):
+def corr_distances(mat1, mat2, plot_out=True):
     '''
     Calculate correlation of two distance matrices using mantel test. 
     Returns stats and plot of distances. mat1 & mat2 are distance matrices.
@@ -48,7 +53,7 @@ def corr_distances(mat1, mat2, plot_out = True):
         
     return mantel_stats
 
-def arcsinh_norm(x, norm = True, scale = 1000):
+def arcsinh_norm(x, norm=True, scale=1000):
     '''
     Perform an arcsinh-transformation on a np.ndarray containing raw data of shape=(n_cells,n_genes).
     Useful for feeding into PCA or tSNE.
@@ -100,4 +105,66 @@ def compare_euclid(pre, post, plot_out=True):
         
     return mantel_stats, EMD
     
+def fcc_PCA(x, n_comp, plot_out=True):
+    '''perform PCA with n_components on matrix x of shape (n_cells, n_features)'''
+    # fit PCA to data
+    PCA_fit = PCA(n_components=n_comp).fit(x)
+    # transform data to fit
+    PCA_results = PCA_fit.transform(x)
+    # plot PCA if desired
+    if plot_out:
+        plt.figure(figsize=(10,5))
+        
+        plt.subplot(121)
+        sns.scatterplot(PCA_results[:,0], PCA_results[:,1], s=75)
+        plt.ylabel('PC2')
+        plt.xlabel('PC1')
+        plt.title('PCA')
+        
+        plt.subplot(122)
+        plt.plot(np.cumsum(np.round(PCA_fit.explained_variance_ratio_, decimals=3)*100))
+        plt.ylabel('% Variance Explained')
+        plt.xlabel('# of Features')
+        plt.title('PCA Analysis')
+        
+        sns.despine(left=True, bottom=True)
+        plt.tight_layout()
+        plt.show()
+        plt.close()
+        
+    return PCA_results
+    
+def fcc_tSNE(x, plot_out=True):
+    '''perform t-SNE on matrix x of shape (n_cells, n_features) to reduce to 2 features'''
+    tSNE_results = TSNE(n_components=2).fit_transform(x)
+    # plot t-SNE if desired
+    if plot_out:
+        plt.figure(figsize=(5,5))
+        sns.scatterplot(tSNE_results[:,0], tSNE_results[:,1], s=75)
+        plt.xlabel('t-SNE 1')
+        plt.ylabel('t-SNE 2')
+        plt.tick_params(labelbottom=False, labelleft=False)
+        sns.despine(left=True, bottom=True)
+        plt.tight_layout()
+        plt.show()
+        plt.close()
+        
+    return tSNE_results
+
+def fcc_DCA(x, n_threads=2, dca_norm=True):
+    '''
+    Use deep count autoencoder (https://github.com/theislab/dca) to denoise and preprocess matrix x of shape (n_cells, n_features).
+    NOTE: DCA removes features with 0 counts for all cells prior to processing.
+    '''
+    adata = scanpy.AnnData(x) # generate AnnData object (https://github.com/theislab/scanpy) for passing to DCA
+    scanpy.pp.filter_genes(adata, min_counts=1) # remove features with 0 counts for all cells
+    dca(adata, threads=n_threads) # perform DCA analysis on AnnData object
+    
+    if dca_norm:
+        scanpy.pp.normalize_per_cell(adata) # normalize features for each cell with scanpy's method
+        scanpy.pp.log1p(adata) # log-transform data with scanpy's method
+        
+    return adata.X # return the denoised data as a np.ndarray
+
+
     
