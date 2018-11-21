39a8806ef1d889d465a809d6568f3a1f7c421fe6
Cody Heiser <codyheiser49@gmail.com>
2018-11-20 18:13:57 -0600

    working with pydpc clustering and distance metrics for a couple datasets

--- fcc_utils.py
+++ fcc_utils.py
@@ -1,4 +1,4 @@
-# t-SNE generation utility functions
+# furry couscous utility functions
 
 # @author: C Heiser
 # November 2018
@@ -6,14 +6,20 @@
 import numpy as np
 import scipy as sc
 # scikit packages
-import skbio
+from skbio.stats.distance import mantel      	# Mantel test for correlation of Euclidean distance matrices
 from sklearn.preprocessing import normalize
-from sklearn.decomposition import PCA        # PCA
-from sklearn.manifold import TSNE            # t-SNE
+from sklearn.decomposition import PCA        	# PCA
+from sklearn.manifold import TSNE            	# t-SNE
+from sklearn.metrics import silhouette_score 	# silhouette score for clustering
+# density peak clustering
+from pydpc import Cluster                    	# density-peak clustering
 # DCA packages
 import scanpy.api as scanpy
-from dca.api import dca                      # DCA
+from dca.api import dca                      	# DCA
+# UMAP
+import umap                                  	# UMAP
 # plotting packages
+import matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sns; sns.set(style = 'white')
 # package for reading in data files
@@ -28,30 +34,6 @@ def read_hdf5(filename):
         
     hdf5in.close()
     return hdf5out
-
-def corr_distances(mat1, mat2, plot_out=True):
-    '''
-    Calculate correlation of two distance matrices using mantel test. 
-    Returns stats and plot of distances. mat1 & mat2 are distance matrices.
-    Output is list of R, p, and n values in order.
-    '''
-    # calculate Spearman correlation coefficient and p-value for distance matrices using Mantel test
-    mantel_stats = skbio.stats.distance.mantel(x=mat1, y=mat2)
-    
-    if plot_out:
-        # for each matrix, take the upper triangle (it's symmetrical), and remove all zeros for plotting distance differences
-        mat1_flat = np.triu(mat1)[np.nonzero(np.triu(mat1))]
-        mat2_flat = np.triu(mat2)[np.nonzero(np.triu(mat2))]
-        
-        plt.figure(figsize=(5,5))
-        sns.scatterplot(mat1_flat, mat2_flat, s=75, alpha=0.6)
-        plt.figtext(0.95, 0.5, 'R: {}\np-val: {}\nn: {}'.format(round(mantel_stats[0],5),mantel_stats[1],mantel_stats[2]), fontsize=12)
-        plt.title('Distance Correlation')
-        sns.despine(left=True, bottom=True)
-        plt.tight_layout()
-        plt.show()
-        
-    return mantel_stats
 
 def arcsinh_norm(x, norm=True, scale=1000):
     '''
@@ -79,31 +61,81 @@ def log2_norm(x, norm = True):
         return np.log2(normalize(x, axis=0, norm='l2') + 1)
     
 def compare_euclid(pre, post, plot_out=True):
-    '''test for correlation between Euclidean cell-cell distances before and after transformation by a function or DR algorithm'''
+    '''
+    Test for correlation between Euclidean cell-cell distances before and after transformation by a function or DR algorithm.
+    1) calculates Euclidean distance matrix (n_cells, n_cells)
+    2) performs Mantel test for correlation of distance matrices (skbio.stats.distance.mantel())
+    3) normalizes unique distances (upper triangle of distance matrix) to maximum for each dataset, yielding fractions in range [0, 1]
+    4) calculates Earth-Mover's Distance and Kullback-Leibler Divergence for euclidean distance fractions between datasets
+    5) plots fractional Euclidean distances for both datasets, cumulative probability distribution for fractional distances, and correlation of distances in one figure
+    
+        pre = matrix of shape (n_cells, n_features) before transformation/projection
+        post = matrix of shape (n_cells, n_features) after transformation/projection
+        plot_out = print plots as well as return stats?
+    '''
+    # make sure the number of cells in each matrix is the same
+    assert pre.shape[0] == post.shape[0] , 'Matrices contain different number of cells.\n{} in "pre"\n{} in "post"\n'.format(pre.shape[0], post.shape[0])
+    
     # generate distance matrices for pre- and post-transformation arrays
     dm_pre = sc.spatial.distance_matrix(pre,pre)
     dm_post = sc.spatial.distance_matrix(post,post)
     
     # calculate Spearman correlation coefficient and p-value for distance matrices using Mantel test
-    mantel_stats = skbio.stats.distance.mantel(x=dm_pre, y=dm_post)
+    mantel_stats = mantel(x=dm_pre, y=dm_post)
     
-    # for each matrix, take the upper triangle (it's symmetrical), and remove all zeros for calculating EMD and plotting distance differences
-    pre_flat = np.triu(dm_pre)[np.nonzero(np.triu(dm_pre))]
-    post_flat = np.triu(dm_post)[np.nonzero(np.triu(dm_post))]
+    # for each matrix, take the upper triangle (it's symmetrical) for calculating EMD and plotting distance differences
+    pre_flat = dm_pre[np.triu_indices(dm_pre.shape[1],1)]
+    post_flat = dm_post[np.triu_indices(dm_post.shape[1],1)]
+    
+    # normalize flattened distances within each set for fair comparison of probability distributions
+    pre_flat_norm = (pre_flat/pre_flat.max())
+    post_flat_norm = (post_flat/post_flat.max())
     
     # calculate EMD for the distance matrices
-    EMD = sc.stats.wasserstein_distance(pre_flat.flatten(), post_flat.flatten())
+    EMD = sc.stats.wasserstein_distance(pre_flat_norm, post_flat_norm)
+    
+    # Kullback Leibler divergence
+    # add very small number to avoid dividing by zero
+    KLD = sc.stats.entropy(pre_flat_norm+0.00000001, post_flat_norm+0.00000001)
     
     if plot_out:
-        plt.figure(figsize=(5,5))
-        sns.scatterplot(pre_flat, post_flat, s=75, alpha=0.6)
-        plt.figtext(0.95, 0.5, 'R: {}\np-val: {}\nn: {}\nEMD: {}'.format(round(mantel_stats[0],5),mantel_stats[1],mantel_stats[2],round(EMD,5)), fontsize=12)
-        plt.title('Distance Correlation')
-        sns.despine(left=True, bottom=True)
+        plt.figure(figsize=(15,5))
+        
+        plt.subplot(131)
+        # plot unique cell-cell distances
+        plt.plot(pre_flat_norm, alpha=0.5, label='pre')
+        plt.plot(post_flat_norm, alpha=0.5, label='post')
+        plt.title('Normalized Unique Distances', fontsize=16)
+        plt.legend(loc='best',fontsize=14)
+        plt.tick_params(labelsize=12, labelbottom=False)
+        
+        plt.subplot(132)
+        # calculate and plot the cumulative probability distributions for cell-cell distances in each dataset
+        num_bins = int(len(pre_flat_norm)/100)
+        pre_counts, pre_bin_edges = np.histogram (pre_flat_norm, bins=num_bins)
+        pre_cdf = np.cumsum (pre_counts)
+        post_counts, post_bin_edges = np.histogram (post_flat_norm, bins=num_bins)
+        post_cdf = np.cumsum (post_counts)
+        plt.plot(pre_bin_edges[1:], pre_cdf/pre_cdf[-1], label='pre')
+        plt.plot(post_bin_edges[1:], post_cdf/post_cdf[-1], label='post')
+        plt.title('Cumulative Probability of Normalized Distances', fontsize=16)
+        plt.legend(loc='best',fontsize=14)
+        plt.tick_params(labelsize=12)
+        
+        plt.subplot(133)
+        # plot correlation of distances
+        sns.scatterplot(pre_flat_norm, post_flat_norm, s=75, alpha=0.5)
+        plt.figtext(0.95, 0.5, 'R: {}\np-val: {}\nn: {}\n\nEMD: {}\nKLD: {}'.format(round(mantel_stats[0],5),mantel_stats[1],mantel_stats[2],round(EMD,4),round(KLD,4)), fontsize=14)
+        plt.title('Normalized Distance Correlation', fontsize=16)
+        plt.xlabel('Pre-Transformation', fontsize=14)
+        plt.ylabel('Post-Transformation', fontsize=14)
+        plt.tick_params(labelleft=False, labelbottom=False)
+        
+        sns.despine()
         plt.tight_layout()
         plt.show()
         
-    return mantel_stats, EMD
+    return mantel_stats, EMD, KLD
     
 def fcc_PCA(x, n_comp, plot_out=True):
     '''perform PCA with n_components on matrix x of shape (n_cells, n_features)'''
@@ -116,40 +148,25 @@ def fcc_PCA(x, n_comp, plot_out=True):
         plt.figure(figsize=(10,5))
         
         plt.subplot(121)
-        sns.scatterplot(PCA_results[:,0], PCA_results[:,1], s=75)
-        plt.ylabel('PC2')
-        plt.xlabel('PC1')
-        plt.title('PCA')
+        sns.scatterplot(PCA_results[:,0], PCA_results[:,1], s=75, alpha=0.7)
+        plt.tick_params(labelbottom=False, labelleft=False)
+        plt.ylabel('PC2', fontsize=14)
+        plt.xlabel('PC1', fontsize=14)
+        plt.title('PCA', fontsize=16)
         
         plt.subplot(122)
         plt.plot(np.cumsum(np.round(PCA_fit.explained_variance_ratio_, decimals=3)*100))
-        plt.ylabel('% Variance Explained')
-        plt.xlabel('# of Features')
-        plt.title('PCA Analysis')
+        plt.tick_params(labelsize=12)
+        plt.ylabel('% Variance Explained', fontsize=14)
+        plt.xlabel('# of Features', fontsize=14)
+        plt.title('PCA Analysis', fontsize=16)
         
-        sns.despine(left=True, bottom=True)
+        sns.despine()
         plt.tight_layout()
         plt.show()
         plt.close()
         
     return PCA_results
-    
-def fcc_tSNE(x, plot_out=True):
-    '''perform t-SNE on matrix x of shape (n_cells, n_features) to reduce to 2 features'''
-    tSNE_results = TSNE(n_components=2).fit_transform(x)
-    # plot t-SNE if desired
-    if plot_out:
-        plt.figure(figsize=(5,5))
-        sns.scatterplot(tSNE_results[:,0], tSNE_results[:,1], s=75)
-        plt.xlabel('t-SNE 1')
-        plt.ylabel('t-SNE 2')
-        plt.tick_params(labelbottom=False, labelleft=False)
-        sns.despine(left=True, bottom=True)
-        plt.tight_layout()
-        plt.show()
-        plt.close()
-        
-    return tSNE_results
 
 def fcc_DCA(x, n_threads=2, dca_norm=True):
     '''
@@ -166,5 +183,81 @@ def fcc_DCA(x, n_threads=2, dca_norm=True):
         
     return adata.X # return the denoised data as a np.ndarray
 
+def fcc_tSNE(x, p = 30, plot_out=True):
+    '''perform t-SNE with perplexity p on matrix x of shape (n_cells, n_features) to reduce to 2 features'''
+    tSNE_results = TSNE(n_components=2, perplexity=p).fit_transform(x)
+    # plot t-SNE if desired
+    if plot_out:
+        plt.figure(figsize=(5,5))
+        sns.scatterplot(tSNE_results[:,0], tSNE_results[:,1], s=75, alpha=0.7)
+        plt.xlabel('t-SNE 1', fontsize=14)
+        plt.ylabel('t-SNE 2', fontsize=14)
+        plt.tick_params(labelbottom=False, labelleft=False)
+        sns.despine(left=True, bottom=True)
+        plt.tight_layout()
+        plt.show()
+        plt.close()
+        
+    return tSNE_results
 
+def fcc_UMAP(x, p = 30, min_dist=0.3, plot_out=True):
+    '''
+    perform UMAP with perplexity p and minimum distance min_dist on matrix x of shape (n_cells, n_features) 
+    to reduce to 2 features
+    '''
+    UMAP_results = umap.UMAP(n_neighbors=p, min_dist=min_dist, metric='correlation').fit_transform(x)
+    # plot t-SNE if desired
+    if plot_out:
+        plt.figure(figsize=(5,5))
+        sns.scatterplot(UMAP_results[:,0], UMAP_results[:,1], s=75, alpha=0.7)
+        plt.xlabel('UMAP 1', fontsize=14)
+        plt.ylabel('UMAP 2', fontsize=14)
+        plt.tick_params(labelbottom=False, labelleft=False)
+        sns.despine(left=True, bottom=True)
+        plt.tight_layout()
+        plt.show()
+        plt.close()
+        
+    return UMAP_results
+
+# WORK IN PROGRESS
+def gen_clusters(x, plot_out=True):
+    '''
+    Use pydpc to cluster matrix x of shape (n_cells, n_features). 
+    You will be asked for density and delta cutoffs to determine cluster centers.
+    Returns pydpc cluster object and silhouette score for clustering.
+    '''
+    matplotlib.use('agg')
+    # output delta vs density plot of points
+    clu = Cluster(x)
+    clu.draw_decision_graph()
     
+    # prompt user for cutoff values
+    dens = float(input('Density Cutoff Value: '))
+    delt = float(input('Delta Cutoff Value: '))
+    
+    # assign cluster centers and output plot
+    clu.assign(dens, delt)
+    
+    if plot_out:
+        # plot clusters with point densities
+        fig, ax = plt.subplots(1, 3, figsize=(15, 5))
+        ax[0].scatter(x[:, 0], x[:, 1], s=75, alpha=0.7)
+        ax[0].scatter(x[clu.clusters, 0], x[clu.clusters, 1], s=90, c="red")
+        ax[1].scatter(x[:, 0], x[:, 1], s=75, c=clu.density)
+        ax[2].scatter(x[:, 0], x[:, 1], s=75, c=clu.membership, cmap=plt.cm.plasma, alpha=0.7)
+        for _ax in ax:
+            _ax.set_aspect('equal')
+            _ax.tick_params(labelbottom=False, labelleft=False)
+        
+        sns.despine(left=True, bottom=True)
+        fig.tight_layout()
+    
+    # calculate silhouette score
+    if clu.membership.max()==0:
+        ss = 0
+    
+    else:
+        ss = silhouette_score(x, clu.membership)
+    
+    return clu, ss
