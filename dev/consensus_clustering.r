3020bd0ac8e89a80685f04514e18ee583d49a3d7
Cody Heiser <codyheiser49@gmail.com>
2019-05-28 18:27:59 -0500

    final consensus clustering using Seurat Louvain alg. re-running R methods with new clustering.

--- dev/consensus_clustering.r
+++ dev/consensus_clustering.r
@@ -11,14 +11,17 @@ colon_seurat %>%
   RunPCA() %>%
   FindNeighbors() %>%
   FindClusters(resolution=0.25) %>%
-  RunTSNE(dims = 1:10, seed.use = 18, perplexity=30) -> colon_seurat
+  RunTSNE(dims = 1:100, seed.use = 18, perplexity=30) -> colon_seurat
 
 # visualize clusters with t-SNE
 DimPlot(colon_seurat, reduction = "tsne")
 
 # create heatmap of upregulated cluster markers
-colon.markers <- FindAllMarkers(colon_seurat, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
-top10 <- colon.markers %>% group_by(cluster) %>% top_n(n = 10, wt = avg_logFC)
+colon.markers <- FindAllMarkers(colon_seurat, min.pct = 0.25, logfc.threshold = 0.25)
+top10 <- colon.markers %>% 
+  filter(!str_detect(gene, pattern = 'mt-')) %>% 
+  group_by(cluster) %>% 
+  top_n(n = 10, wt = avg_logFC)
 DoHeatmap(colon_seurat, features = top10$gene) + NoLegend()
 
 # export clusters to file
@@ -27,7 +30,11 @@ write.table(colon_seurat@reductions$tsne@cell.embeddings, file = 'dev/Rmethods_o
 
 ###################################################################################################################################
 # discrete data - GEO accession # GSM1626793
-retina <- t(read.csv('inputs/GSM1626793_P14Retina_1.processed.tsv', sep='\t', header = T, row.names = 1, check.names = F))
+retina <- data.frame(t(read.csv('inputs/GSM1626793_P14Retina_1.processed.tsv', sep='\t', header = T, row.names = 1, check.names = F))) %>%
+  rownames_to_column(var='gene') %>%
+  separate(gene, into = c('chr','pos','gene'), sep = ':', remove=T) %>%
+  column_to_rownames('gene') %>%
+  select(-c(chr, pos))
 CreateSeuratObject(counts = retina) %>% FindVariableFeatures() -> retina_seurat
 
 # process data and find clusters using graph-based method
@@ -35,17 +42,20 @@ retina_seurat %>%
   ScaleData() %>%
   RunPCA() %>%
   FindNeighbors() %>%
-  FindClusters(resolution=0.3) %>%
-  RunTSNE(dims = 1:10, seed.use = 18, perplexity=30) -> retina_seurat
+  FindClusters(resolution=0.25) %>%
+  RunTSNE(dims = 1:100, seed.use = 18, perplexity=30) -> retina_seurat
 
 # visualize clusters with t-SNE
 DimPlot(retina_seurat, reduction = "tsne")
 
 # create heatmap of upregulated cluster markers
-retina.markers <- FindAllMarkers(retina_seurat, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
-top10 <- retina.markers %>% group_by(cluster) %>% top_n(n = 10, wt = avg_logFC)
+retina.markers <- FindAllMarkers(retina_seurat, min.pct = 0.25, logfc.threshold = 0.25)
+top10 <- retina.markers %>% 
+  filter(!str_detect(gene, pattern = 'mt-')) %>% 
+  group_by(cluster) %>% 
+  top_n(n = 10, wt = avg_logFC) 
 DoHeatmap(retina_seurat, features = top10$gene) + NoLegend()
 
-# export clusters and UMAP projection to file
+# export clusters and t-SNE projection to file
 write.table(retina_seurat@meta.data$seurat_clusters, file = 'inputs/retina_clu_seurat.csv', row.names = F, col.names = F)
 write.table(retina_seurat@reductions$tsne@cell.embeddings, file = 'dev/Rmethods_out/retina_seurat_tSNE.csv', row.names = T, col.names = T, sep = ',')
