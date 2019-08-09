f4868e0ed4dc17f83618482b0e97c4ac1f9d5055
Cody Heiser <codyheiser49@gmail.com>
2019-08-09 17:03:30 -0500

    fixed and simplified cell type prediction function

--- fcc_utils.r
+++ fcc_utils.r
@@ -81,20 +81,14 @@ seurat.pipe <- function(counts, n.pcs=100, k=30, tsne=T, umap=F, perplexity=30, 
 }
 
 
-fcc_predictcelltype<-function(qdata, ref.expr, anntext="Query", normalize=FALSE, corcutoff=0){
+fcc_predictcelltype<-function(qdata, ref.expr, anntext="Query", corcutoff=0){
   # predict cell types based on expression correlation with reference database
   # adapted from scUnifrac (https://github.com/liuqivandy/scUnifrac)
   #   qdata = matrix of counts in cells x genes format, with cell and gene labels as rownames and colnames, respectively
   #   ref.expr = matrix of expression by cell type from reference database. 
   #              can be loaded using `load(system.file("extdata", "ref.expr.Rdata", package = "scUnifrac"))`
   #   anntext = string to annotate output heatmap with
-  #   normalize = normalize expression counts prior to correlating with reference?
   #   corcutoff = minimum correlation value to require before returning cell type prediction
-  if (normalize){
-    sumqdata<-apply(qdata,2,sum)
-    # normalize the data    
-    normdata<-t(log2(t(qdata)/sumqdata*10000+1))     
-  }
   
   commongene<-intersect(rownames(qdata),rownames(ref.expr))
   
@@ -107,72 +101,19 @@ fcc_predictcelltype<-function(qdata, ref.expr, anntext="Query", normalize=FALSE,
     
     cors_index <- unlist(apply(cors,2,function(x){cutoffind<-tail(sort(x),3)>corcutoff;return(order(x,decreasing=T)[1:3][cutoffind])}))
     
-    if (length(cors_index)>1){
-      
-      cors_in = cors[cors_index,]
-      
-      colnames(cors_in)<-colnames(qdata)
-      
-      rhc<-hclust(dist(t(cors_in)),method="ward.D2")
-      hc<-hclust(dist((cors_in)),method="ward.D2")
-      rownum<-nrow(cors_in)
-      colnum<-ncol(cors_in)
-      layout(matrix(c(4,2,3,1), 2, 2, byrow = TRUE),width=c(1,6),heights=c(1,6))
-      par(mar=c(3,1,1,10))
-      par(xaxs="i")
-      par(yaxs="i")
-      image(1:colnum,1:rownum,t(cors_in[hc$order,rhc$order]),col=colorRampPalette(c("gray","white","red"))(100),axes=F) 
-      rowcex<-min(0.6, 50 / rownum * 0.6)
-      axis(4,1:rownum,labels=rownames(cors_in)[hc$order],las=2,tick=F,cex.axis=rowcex)
-      mtext(anntext,side=1,line=1,at=colnum/2,cex=1.5)
-      par(mar=c(0,1,1,10))
-      plot(as.dendrogram(rhc),axes=F,leaflab="none")
-      par(mar=c(3,1,1,0))
-      plot(as.dendrogram(hc),horiz=T,axes=F,leaflab="none")
-      par(mar=c(2,1,2,1))
-      maxval<-round(max(cors_in),digits=1)
-      zval<-seq(0,maxval,0.1)
-      image(1:length(zval),1,matrix(zval,ncol=1),col= colorRampPalette(c("gray","white", "red"))(100),axes=F,xlab="",ylab="")
-      mtext("0",side=1,line=0.5,at=1,cex=0.8)
-      mtext(maxval,side=1,line=0.5,at=length(zval),cex=0.8)
-      
-      box(lty="solid",col="black")
-      
-      return(cors_in)
-      
-    } else if (length(cors_index)==1) {
-      
-      cors_in=matrix(cors[cors_index,],nrow=1)
-      colnames(cors_in)<-colnames(qdata)	
-      rownames(cors_in)<-rownames(cors)[cors_index]		
-      rownum<-nrow(cors_in)
-      colnum<-ncol(cors_in)
-      rhc<-hclust(dist(t(cors_in)),method="ward.D2")
-      layout(matrix(c(3,2,0,1), 2, 2, byrow = TRUE),width=c(1,6),heights=c(1,6))
-      par(mar=c(3,1,1,10))
-      par(xaxs="i")
-      par(yaxs="i")
-      image(1:colnum,1:rownum,t(t(cors_in[,rhc$order])),col=colorRampPalette(c("gray","white","red"))(100),axes=F,xlab="",ylab="") 
-      rowcex<-min(0.6, 50 / rownum * 0.6)
-      axis(4,1:rownum,labels=rownames(cors_in),las=2,tick=F,cex.axis=rowcex)
-      mtext(anntext,side=1,line=1,at=colnum/2,cex=1.5)
-      par(mar=c(0,1,1,10))
-      plot(as.dendrogram(rhc),axes=F,leaflab="none")
-      
-      par(mar=c(2,1,2,1))
-      maxval<-round(max(cors_in),digits=1)
-      zval<-seq(0,maxval,0.1)
-      image(1:length(zval),1,matrix(zval,ncol=1),col= colorRampPalette(c("gray","white", "red"))(100),axes=F,xlab="",ylab="")
-      mtext("0",side=1,line=0.5,at=1,cex=0.8)
-      mtext(maxval,side=1,line=0.5,at=length(zval),cex=0.8)
-      
-      box(lty="solid",col="black")
-      
-      return(cors_in)
-      
-    }
+    cors_in <- cors[cors_index,]
+    cors_in <- cors_in[unique(rownames(cors_in)),]
+    
+    tryCatch(
+      {
+        heatmap(cors_in, labCol=F, margins=c(0.5,10), cexRow=0.7)
+        return(cors_in)
+      }, error=function(cond) {
+        message(paste0('No cell type correlations detected above ', corcutoff))
+        message(paste0(cond,'\n'))
+      }, warning=function(cond) {
+        message(paste0(cond,'\n'))
+      }
+    )
   }
-  
-  print(paste0('No cell type correlations detected above ', corcutoff))
-  
 }
