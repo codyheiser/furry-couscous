a5725c46088f24f10bc220985ccf1bdf51ddb24d
kenlaulab <ken.s.lau@vanderbilt.edu>
2019-11-05 14:27:23 -0600

    metric and force_recalc args to structure_preservation_sc().

--- fcc_utils.py
+++ fcc_utils.py
@@ -256,16 +256,18 @@ def knn_preservation(pre, post):
     return np.round(100 - ((pre != post).sum()/(pre.shape[0]**2))*100, 4)
 
 
-def structure_preservation_sc(adata, latent, native='X', k=30, downsample=False, verbose=True):
+def structure_preservation_sc(adata, latent, native='X', metric=None, k=30, downsample=False, verbose=True, force_recalc=False):
     '''
     wrapper function for full structural preservation workflow applied to scanpy AnnData object
         adata = AnnData object with latent space to test in .obsm slot, and native (reference) space in .X or .obsm
         latent = adata.obsm key that contains low-dimensional latent space for testing
         native = adata.obsm key or .X containing high-dimensional native space, which should be direct input to dimension reduction
                  that generated latent .obsm for fair comparison. Default 'X', which uses adata.X.
+        metric = distance metric to use. one of ['chebyshev','cityblock','euclidean','minkowski','mahalanobis','seuclidean']. default 'euclidean'.
         k = number of nearest neighbors to test preservation
         downsample = number of distances to downsample to (maximum of 50M [~10k cells, if symmetrical] is recommended for performance)
         verbose = print progress statements
+        force_recalc = if True, recalculate all distances and neighbor graphs, regardless of their presence in AnnData object
     '''
     # 0) determine native space according to argument
     if native == 'X':
@@ -274,26 +276,26 @@ def structure_preservation_sc(adata, latent, native='X', k=30, downsample=False,
         native_space = adata.obsm[native].copy()
     
     # 1) calculate unique cell-cell distances
-    if '{}_distances'.format(native) not in adata.uns.keys(): # check for existence in AnnData to prevent re-work
+    if '{}_distances'.format(native) not in adata.uns.keys() or force_recalc: # check for existence in AnnData to prevent re-work
         if verbose:
             print('Calculating unique distances for native space, {}'.format(native))
-        adata.uns['{}_distances'.format(native)] = pdist(native_space)
+        adata.uns['{}_distances'.format(native)] = pdist(native_space, metric=metric)
     
-    if '{}_distances'.format(latent) not in adata.uns.keys(): # check for existence in AnnData to prevent re-work
+    if '{}_distances'.format(latent) not in adata.uns.keys() or force_recalc: # check for existence in AnnData to prevent re-work
         if verbose:
             print('Calculating unique distances for latent space, {}'.format(latent))
-        adata.uns['{}_distances'.format(latent)] = pdist(adata.obsm[latent])
+        adata.uns['{}_distances'.format(latent)] = pdist(adata.obsm[latent], metric=metric)
     
     # 2) get correlation and EMD values, and return normalized distance vectors for plotting distributions
     adata.uns['{}_norm_distances'.format(native)], adata.uns['{}_norm_distances'.format(latent)], corr_stats, EMD = distance_stats(pre=adata.uns['{}_distances'.format(native)], post=adata.uns['{}_distances'.format(latent)], verbose=verbose, downsample=downsample)
 
     # 3) determine neighbors
-    if '{}_neighbors'.format(native) not in adata.uns.keys(): # check for existence in AnnData to prevent re-work
+    if '{}_neighbors'.format(native) not in adata.uns.keys() or force_recalc: # check for existence in AnnData to prevent re-work
         if verbose:
             print('{}-nearest neighbor calculation for native space, {}'.format(k, native))
         adata.uns['{}_neighbors'.format(native)] = sc.pp.neighbors(adata, n_neighbors=k, use_rep=native, knn=True, metric='euclidean', copy=True).uns['neighbors']
     
-    if '{}_neighbors'.format(latent) not in adata.uns.keys(): # check for existence in AnnData to prevent re-work
+    if '{}_neighbors'.format(latent) not in adata.uns.keys() or force_recalc: # check for existence in AnnData to prevent re-work
         if verbose:
             print('{}-nearest neighbor calculation for latent space, {}'.format(k, latent))
         adata.uns['{}_neighbors'.format(latent)] = sc.pp.neighbors(adata, n_neighbors=k, use_rep=latent, knn=True, metric='euclidean', copy=True).uns['neighbors']
@@ -302,7 +304,7 @@ def structure_preservation_sc(adata, latent, native='X', k=30, downsample=False,
     if verbose:
         print('Determining nearest neighbor preservation')
     if adata.uns['{}_neighbors'.format(native)]['params']['n_neighbors'] != adata.uns['{}_neighbors'.format(latent)]['params']['n_neighbors']:
-        warnings.warn('Warning: Nearest-neighbor graphs constructed with different k values.\n\tk={} in "{}_neighbors", while k={} in "{}_neighbors."\n\tConsider re-generating neighbors graphs.'.format(adata.uns['{}_neighbors'.format(native)]['params']['n_neighbors'], native, adata.uns['{}_neighbors'.format(latent)]['params']['n_neighbors'], latent))
+        warnings.warn('Warning: Nearest-neighbor graphs constructed with different k values. k={} in "{}_neighbors", while k={} in "{}_neighbors". Consider re-generating neighbors graphs by setting force_recalc=True.'.format(adata.uns['{}_neighbors'.format(native)]['params']['n_neighbors'], native, adata.uns['{}_neighbors'.format(latent)]['params']['n_neighbors'], latent))
     knn_pres = knn_preservation(pre=adata.uns['{}_neighbors'.format(native)]['distances'], post=adata.uns['{}_neighbors'.format(latent)]['distances'])
 
     if verbose:
